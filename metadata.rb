7603c686c2f7de39dc5f5c633955011e8e1c8749
Mike Heffner <mikeh@fesnel.com>
2011-12-08 10:34:20 -0500

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "mike@librato.com"
 license          "Apache 2.0"
 description      "Installs/Configures statsd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
 depends "build-essential"
 depends "git"

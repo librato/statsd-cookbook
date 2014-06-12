bc3a647a6c67fefd21e03ac5aa4f5381064a5a7c
Stephan Budge <stephan@stephanx.net>
2014-06-11 18:30:25 -0700

    revert user for merge

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,8 +6,8 @@ default[:statsd][:log_file] = "/var/log/statsd.log"
 default[:statsd][:pid_dir] = "/var/run/statsd"
 default[:statsd][:pid_file] = "/var/run/statsd/statsd.pid"
 
-default[:statsd][:user] = "www-data"
-default[:statsd][:group] = "logs"
+default[:statsd][:user] = "statsd"
+default[:statsd][:group] = "statsd"
 
 default[:statsd][:flush_interval_msecs] = 10000
 default[:statsd][:port] = 8125
